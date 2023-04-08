7be3d42111ab7098cab04c20b7afe336675a3ab4
George Drummond <georgedrummond@gmail.com>
2023-04-07 20:37:08 -0400

    Gem loads now

--- rubygem/lib/wikipaddle_api.rb
+++ rubygem/lib/wikipaddle_api.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require "rack"
+
 require_relative "wikipaddle_api/version"
+require_relative "../rpc/wikipaddle_twirp"
 
 module WikipaddleApi
   class Error < StandardError; end
